35f570dbb3be6ea1793b6bb4b6630e8ad53bb06d
James Marr <james.e.marr@gmail.com>
2008-09-16 07:01:03 +0000

    Added fix to windows for new input processing. It now requires serial numbers for each touch.
    git-svn-id: http://www.piratestudios.com/pammo/svn@329 fc494e2b-ab4b-0410-a0f4-e0a3d7fb9d21

--- client/Source/win32/main.cpp
+++ client/Source/win32/main.cpp
@@ -44,6 +44,9 @@ public:
         Touch touch[5];
         float keyRange = 40;
         
+        touch[0].mSerialNumber = 1;
+        touch[1].mSerialNumber = 2;
+        
 
         bool altUp = false;
         bool mouseUp = false;
